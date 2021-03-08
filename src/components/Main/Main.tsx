4175b560f5594a32d9882874f2727c8ee48ebcdf
Patrick Santos <patrickspteixeira@gmail.com>
2021-03-08 12:01:17 -0300

    feat: remove unwanted boilerplate code

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,22 +1,5 @@
-import * as Styled from './styles'
-
 function Main() {
-  return (
-    <Styled.Wrapper>
-      <Styled.Logo
-        src="/img/logo.svg"
-        alt="Imagem de um átomo e React Avançado escrito ao lado."
-      />
-      <Styled.Title>React Avançado</Styled.Title>
-      <Styled.Description>
-        TypeScript, ReactJS, NextJS e Styled Components
-      </Styled.Description>
-      <Styled.Illustration
-        src="img/hero-illustration.svg"
-        alt="Um desenvolvedor de frente para uma tela com código."
-      />
-    </Styled.Wrapper>
-  )
+  return <div>Next.js na prática</div>
 }
 
 export default Main
